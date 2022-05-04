7eefb224a86cbb063a9dd45baf8518f1db282d2d
sher0501 <asoggamer@gmail.com>
2022-05-04 19:21:20 +0500

    day4-DOM

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+let one = document.getElementById("age-table");
+let two = document.getElementById("age-list").children;
+let three = document.querySelector("td");
+let four = document.querySelector("[type='submit']");
+let five = document.querySelector("input");
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+console.log(five);
